f8e63536e87d215e4d92fc0a62e8e72ea3886d2c
v1605 <55302877+v1605@users.noreply.github.com>
2025-11-01 17:02:52 -0400

    Update ZaparooFloppy.hpp
    Updated hpp for write flag value

--- ZaparooFloppy.hpp
+++ ZaparooFloppy.hpp
@@ -2,3 +2,6 @@
 
 //Move the track to the indicated after reading a disc (helps make more noise). Should be set less than 70
 #define TARGET_TRACK 10
+
+//Some drives are invereted and allways show write protected until a disk is inserted. Change to 0 if that is the case.
+#define WRITE_FLAG 1
